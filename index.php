cacc6ed8b05ca39ce08ba485baf66161a44de6d5
AIA DARCK <126428753+aiadarck@users.noreply.github.com>
2023-10-01 02:43:33 +0330

    index.php

--- index.php
+++ index.php
@@ -3,22 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>سلام دنیا با PHP</title>
+    <title>صفحه وب بدون امکان Inspect</title>
+    <style>
+        /* کد CSS آمده در اینجا به طور فشرده شده است */
+        .a{color:#333;display:block;text-decoration:none;width:100px;height:40px;line-height:40px;background-color:#f0f0f0;border:1px solid #ccc;text-align:center}.a:hover{background-color:#e0e0e0}
+    </style>
 </head>
 <body>
-    <?php
-    // چک کردن اگر فرم ارسال شده است
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // دریافت مقدار فیلد متنی از فرم
-        $message = $_POST["message"];
-        // نمایش متن "سلام دنیا" با مقدار وارد شده توسط کاربر
-        echo "<h2>سلام دنیا، $message!</h2>";
-    }
-    ?>
-    <form method="post">
-        <label for="message">متن خود را وارد کنید:</label>
-        <input type="text" id="message" name="message" required>
-        <button type="submit">ارسال</button>
-    </form>
+    <a class="a" href="#">لینک</a>
 </body>
 </html>
